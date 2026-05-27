b0ae7ae5249ae012c9ea34af171df447a23bf383
old-head-dev <38575614+old-head-dev@users.noreply.github.com>
2026-05-27 18:43:27 -0400

    Scaffold NYCTrip2026 Xcode project (iOS 17+, SwiftUI, Swift Testing, bundle com.jonathanhering.NYCTrip2026)
    Created via Xcode UI by Jon. Synchronized file system groups (objectVersion 77)
    mean source folders auto-sync into the build target.
    
    Schemes: NYCTrip2026 (covers app + Tests + UITests targets)
    Bundle ID: com.jonathanhering.NYCTrip2026
    Team: 7Q9C65D6KU (Jonathan Hering)
    Deployment: iOS 17.6
    Baseline build green on iPhone 17 Pro simulator (iOS 26.5).

---
+++ NYCTrip2026/NYCTrip2026/NYCTrip2026App.swift
@@ -0,0 +1,17 @@
+//
+//  NYCTrip2026App.swift
+//  NYCTrip2026
+//
+//  Created by Jon Hering on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct NYCTrip2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NYCTrip2026/NYCTrip2026Tests/NYCTrip2026Tests.swift
@@ -0,0 +1,19 @@
+//
+//  NYCTrip2026Tests.swift
+//  NYCTrip2026Tests
+//
+//  Created by Jon Hering on 5/27/26.
+//
+
+import Testing
+@testable import NYCTrip2026
+
+struct NYCTrip2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}

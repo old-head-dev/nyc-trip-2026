86a441d8f1a44288c3cd9ce26520a1ce5ad05702
old-head-dev <38575614+old-head-dev@users.noreply.github.com>
2026-05-27 20:01:30 -0400

    Wire TripView into app entry point; remove placeholder ContentView

--- NYCTrip2026/NYCTrip2026/NYCTrip2026App.swift
+++ NYCTrip2026/NYCTrip2026/NYCTrip2026App.swift
@@ -1,17 +1,10 @@
-//
-//  NYCTrip2026App.swift
-//  NYCTrip2026
-//
-//  Created by Jon Hering on 5/27/26.
-//
-
 import SwiftUI
 
 @main
 struct NYCTrip2026App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TripView()
         }
     }
 }
